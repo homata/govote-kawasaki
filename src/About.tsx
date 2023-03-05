e3ce4a7fa53ac1786f79eb08f8afeb81b8e66df0
Hiroshi Omata <op.homata@gmail.com>
2023-03-05 17:30:41 +0900

    fixed delete share button error

--- src/About.tsx
+++ src/About.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './About.scss'
 import Qrcode from './Qrcode'
-import Share from './App/Share'
+// import Share from './App/Share'
 import config from './config.json'
 
 const Content = () => {
@@ -26,7 +26,9 @@ const Content = () => {
       <div className="branding">
         <img className="image" src={logoUrl} alt=""/>
       </div>
+
       <div className="description">{config.description}</div>
+      <div className="qrcode"><Qrcode url={window.location.href}/></div>
     </div>
   );
 };
