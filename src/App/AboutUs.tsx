3fa0c2631048114bdbd553dc8a3e252ac8f27338
Hiroshi Omata <op.homata@gmail.com>
2023-03-05 16:10:09 +0900

    Mask Image tab

--- src/App/AboutUs.tsx
+++ src/App/AboutUs.tsx
@@ -1,15 +1,6 @@
 import './AboutUs.scss'
-import config from '../config.json'
-import { FaPlus } from 'react-icons/fa';
 
 const Content = () => {
-  const clickHandler = () => {
-    if (config.form_url) {
-      window.location.href = config.form_url
-    }
-  }
-
-
   return (
     <div className="about-us">
       <div className="container">
